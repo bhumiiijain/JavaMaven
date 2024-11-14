c1b4775c69c8929213ebc44edb2c3ea513c59110
bhumika <bhumiii1910@gmailcom>
2024-11-14 12:20:21 +0530

    SAMPLE MAVEN JAVA

---
+++ src/main/java/com/app/demo/JavaMavenExample/App.java
@@ -0,0 +1,10 @@
+package com.app.demo.JavaMavenExample;
+
+/**
+ * Hello world! clean install test build(goals)
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Bhumika 22BD1A0549!");
+    }
+}
